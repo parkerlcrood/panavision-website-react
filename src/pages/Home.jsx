ef47a2aa1267b346968975a96666bd46348b05fe
parkerlcrood <reedpm03@gmail.com>
2026-07-25 11:45:20 -0700

    changed home screen photo and made it a link to the newest release (beautification, not much programming)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,11 +5,15 @@ import BackToTop from '../components/BackToTop';
 function Home() {
     return <div className="home" id="top">
         <div className="hero">
+            <div className="promoimagecontainer">
+                <a href="https://ffm.to/two-full-steps">
+                    <img className="promoimage" src={`${import.meta.env.BASE_URL}TwoFullSteps.jpeg`}></img>
+                </a>
+            </div>
             <div className="promotextcontainer">
                 <h1 className="promotext">Two Full Steps - New Release</h1>
                 <p id="promostatement">Listen on all platforms to the newest single by Pana-vision, Two Full Steps. More importantly it contains the song 'BigBoyTV.wav', a divergence from their usual sound, a contrast so stark it broke a window in one of the band member's houses (true story).</p>
             </div>
-            <div className="promoimagecontainer"><img className="promoimage" src={`${import.meta.env.BASE_URL}bigboytv.png`}></img></div>
         </div>
         <div className="homelogocontainer"><img className="homepagelogo" src={`${import.meta.env.BASE_URL}PanavisionName.png`}/></div>
         <div className="mysteriousPhrase"><p className="statement">Here, you're on the track to BigBoyTV status...</p></div>
